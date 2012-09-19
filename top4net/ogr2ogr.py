726ffddf89690dec9e3950bdd0baf205a47dd7f6
loic <loic.gasser@citiviz.com>
2012-09-19 12:45:11 +0200

    load sql to database

--- top4net/ogr2ogr.py
+++ top4net/ogr2ogr.py
@@ -18,11 +18,14 @@ def clip_shp_to_shp(directory, shpclippath, pref="", suf="_clip"):
 
 def shp_2_sql(path_to_shape, srs, table_name, path_to_sql):
     import subprocess
-    #subprocess.call(["shp2pgsql", "-s", srs, path_to_shape, table_name, ">", path_to_sql])
     subprocess.call(["sh", "-c", "shp2pgsql -s "+srs+" "+path_to_shape+" "+table_name+" > "+path_to_sql])
 
-def sql_2_postgresql():
-    import subprocess
+def sql_2_postgresql(database,path_to_sql,user='postgres',host='localhost'):
+    from subprocess import Popen, PIPE
+    psql_pass = dict(PGPASSWORD = 'postgres')
+    su_process = Popen("psql -U "+user+" "+"-d "+database+" "+"-h "+host+" "+"-f "+path_to_sql, shell=True, env=psql_pass, stdout=PIPE, stderr=PIPE)
+    data = su_process.stdout.read()
+    #err = su_process.stderr.read()
 
 def reproject():
     import subprocess
