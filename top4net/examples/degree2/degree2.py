2d25e35f4937f15eb1c34869525a759eedcc5055
loic <loic.gasser@citiviz.com>
2012-09-28 15:07:29 +0200

    list edges to heal

--- top4net/examples/degree2/degree2.py
+++ top4net/examples/degree2/degree2.py
@@ -11,20 +11,21 @@ class RemoveDegree2:
         self.G = nx.Graph()
         self.network = Network
         self.query = Session.query(self.network)
-        self.node2 = list(self.getNodeDegree2())
-        self.healEdges()
+        self.node2 = list(self.getEdgesDegree2())
+        #self.healEdges()
 
-    def getNodeDegree2(self):
+    def getEdgesDegree2(self):
         for res in self.query:
             ed = res.compute_results(['id','fn_id','tn_id'])
             self.G.add_edge(ed['fn_id'], ed['tn_id'], eid=ed['id'])
         nodes = self.G.nodes()
         for node in nodes:
             if self.G.degree(node) == 2:
-                yield node
-         
-    def healEdges(self):
-        i = 1
+                edge1, edge2 = self.G.edges(node)
+                yield self.G.get_edge_data(*edge1)['eid'], self.G.get_edge_data(*edge2)['eid']
+                
+    #def healEdges(self):
+        
           
                 
 RemoveDegree2()
